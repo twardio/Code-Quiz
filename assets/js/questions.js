8b8d63ccd65119697d516c4f23dc4b44651b739c
Tom <thomas.f.ward.89@gmail.com>
2023-03-06 15:21:08 +0000

    starter code added

---
+++ assets/js/questions.js
@@ -0,0 +1,31 @@
+
+
+const quizQuestions = [
+    {
+      question: "What is the capital of France?",
+      answers: {
+        a: "London",
+        b: "Paris",
+        c: "Rome"
+      },
+      correctAnswer: 2
+    },
+    {
+      question: "What is the largest planet in our solar system?",
+      answers: {
+        a: "Venus",
+        b: "Jupiter",
+        c: "Saturn"
+      },
+      correctAnswer: 2
+    },
+    {
+      question: "What is the tallest mammal?",
+      answers: {
+        a: "Elephant",
+        b: "Giraffe",
+        c: "Horse"
+      },
+      correctAnswer: 1
+    }
+  ];
